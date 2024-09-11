eecd1533ff80d8ab1adeb21303e7e8f92cc232d1
MaybeRT-rt <maybe.rt@icloud.com>
2024-09-11 12:09:54 +0300

    add pluse and minus button

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -12,18 +12,38 @@ class ViewController: UIViewController {
     var count = 0
     
     @IBOutlet weak var counterLabel: UILabel!
-    
     @IBOutlet weak var clickButton: UIButton!
+    
+    @IBOutlet weak var buttonStack: UIStackView!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var pluseButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        counterLabel.text = "Значение счетчика: \(count)"
+        counterLabel.text = "\(count)"
         clickButton.layer.cornerRadius = 14
+        minusButton.layer.cornerRadius = 14
+        pluseButton.layer.cornerRadius = 14
     }
     
-    
-    @IBAction func tappedCountButton(_ sender: Any) {
+    @IBAction func tappedPluseButton(_ sender: Any) {
         count += 1
-        counterLabel.text = "Значение счетчика: \(count)"
+        counterLabel.text =  "\(count)"
     }
+    
+    @IBAction func tappedMinusButton(_ sender: Any) {
+        guard count != 0 else { return }
+        count -= 1
+        counterLabel.text =  "\(count)"
+    }
+    
+    @IBAction func tappedRefreshButton(_ sender: Any) {
+        count = 0
+        counterLabel.text =  "\(count)"
+    }
+//    @IBAction func tappedCountButton(_ sender: Any) {
+//        count += 1
+//        counterLabel.text =  "\(count)"
+//    }
 }
